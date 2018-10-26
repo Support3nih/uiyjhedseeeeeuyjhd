3e08f22ed5f68178e695118e3c70d82c1ec10eb7
Support3nih <44061131+Support3nih@users.noreply.github.com>
2018-10-27 01:44:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,19 +10,16 @@ console.log('iiFireGame');
 });
 
 
-client.on("guildMemberAdd", msg => {
-    var AlphaE = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .setAuthor(msg.user.username, msg.user.avatarURL)
-  .setThumbnail(msg.user.avatarURL)
-  .setTitle(`
-  مرحبا بك في السيرفر
-  نتمى ان تدخل الي السيرفر
-  `)
-  .addField("هذي الرساله خاصه بك يا حبيبي", `${msg.user.tag}`, true)
-  .addField(`هذا الرابط خاصه بك`,`https://discord.gg/NvNu3xw`, true)
-   .setFooter(msg.user.tag, msg.user.avatarURL, true)
-  msg.user.sendMessage(AlphaE);
-  });
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم 
+لو سمحت ممكن تدخل سيرفرى
+                               [ https://discord.gg/NvNu3xw ] 
+الدعووة خاصة لك ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
 
 client.login("NTA1NDA5NjA4NzA2NDI0ODQz.DrUyUg.2PdG0DuH_BPqiyDbtKV3WTgWOHA");
